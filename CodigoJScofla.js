91ab2da130be1a99c12207cf8ecab2ef633fe702
martin841 <60629322+martin841@users.noreply.github.com>
2021-04-24 22:43:25 -0300

    Add files via upload

---
+++ CodigoJScofla.js
@@ -0,0 +1,92 @@
+
+
+
+//       let cofla  = prompt("cuanto dinero tenes cofla? ");
+
+//  if (cofla >= 1 && cofla <= 2) { alert("tenes un peso y pico")}
+                                    
+//  else if (cofla >= 2 && cofla <= 3){alert("tenes 2 a 3 pesos  ");}
+//  else if (cofla >= 3 && cofla <= 4){alert("tenes 3 a 4 pesos ");}
+//  else if (cofla >= 4 && cofla <= 5){alert("tenes 4 a 5 pesos ");}
+//  else if (cofla >= 5 && cofla <= 6){alert("tenes 5 a 6 pesos ");}
+//  else if (cofla >= 6 && cofla <= 7){alert("tenes 6 a 7 pesos ");}
+//  else if (cofla > 7){ alert("tenes 7 pesos o mas");  }
+                                    
+//   else { alert("no tenes un mango");}
+
+                                    
+
+// let pedro  = prompt("cuanto dinero tenes  pedro? ");
+
+// if (pedro  >= 1 && pedro <= 2) { alert("tenes un peso y pico")}
+
+// else if (pedro >= 2 && pedro <= 3){alert("tenes 2 a 3 pesos  ");}
+// else if (pedro >= 3 && pedro <= 4){alert("tenes 3 a 4 pesos ");}
+// else if (pedro >= 4 && pedro <= 5){alert("tenes 4 a 5 pesos ");}
+// else if (pedro >= 5 && pedro <= 6){alert("tenes 5 a 6 pesos ");}
+// else if (pedro >= 6 && pedro <= 7){alert("tenes 6 a 7 pesos ");}
+// else if (pedro > 7){ alert("tenes 7 pesos o mas");  }
+
+// else { alert("no tenes un mango");}
+
+
+
+
+
+// let juan  = prompt("cuanto dinero tenes juan ? ");
+
+// if (juan >= 1 && juan <= 2) { alert("tenes un peso y pico")}
+
+// else if (juan >= 2 && juan <= 3){alert("tenes 2 a 3 pesos  ");}
+// else if (juan >= 3 && juan <= 4){alert("tenes 3 a 4 pesos ");}
+// else if (juan >= 4 && juan <= 5){alert("tenes 4 a 5 pesos ");}
+// else if (juan >= 5 && juan <= 6){alert("tenes 5 a 6 pesos ");}
+// else if (juan >= 6 && juan <= 7){alert("tenes 6 a 7 pesos ");}
+// else if (juan > 7){ alert("tenes 7 pesos o mas");  }
+
+// else { alert("no tenes un mango");}
+
+/*FIESTA DEL POBRE*/
+
+// let joven = prompt("Biemvenido a la fiesta del pobre,dime tu edad");
+
+
+
+
+
+// while (joven >= 18) {
+
+//     document.write("podes pasar,tenes"  + joven +  " años de edad");
+
+//    hora = prompt ("ingrese su hora de llegada"); 
+
+//    if(hora ==2) {document.write("pasas gratis")}
+
+//    else {document.write ("    pero pagas $1500 pesos") };
+
+//    breack;
+// }
+
+// while(joven > 0 && joven < 18)
+
+// {document.write("no podes pasar,sos joven")
+// breack;}
+
+
+
+
+
+
+
+
+
+// if(joven >= 18)
+// {
+//   document.write("pasa viejo y son 1500 pesos")
+// }
+
+// else if ( hora == 2)
+// {document.write("pasa gratis campeon/a")  }
+
+// else
+// { document.while("no podes pasar") };
