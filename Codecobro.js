d38c2f0156a918c1ecf86a9f6aa0e35c4f98208e
martin841 <martin.v.orellano@gmail.com>
2021-04-26 22:59:17 -0300

    creasion de sistema de cobro con if

---
+++ Codecobro.js
@@ -0,0 +1,487 @@
+
+//Sistema de cobros a deudores con 10% semanal//
+
+deudaprimaria = prompt ("ingrese la deuda actual");
+
+
+semanaderetraso = prompt ("cuanto lleva atrasado");
+let semanaderetraso = parseInt(semanaderetraso);
+
+let deudaprimaria=parseInt(deudaprimaria);
+let deudatotal= parseInt(deudatotal) ;
+
+//Valor de la deuda más el interés = Semanaderetraso //
+
+if (semanaderetraso == 1 ) 
+
+{ Semanaderetraso = ( deudaprimaria * 10 ) / 100 ;
+
+  deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+
+
+else if ( semanaderetraso == 2 ) 
+
+{ semanaderetraso = (deudaprimaria*20)/100;
+
+  deudatotal = ( deudaprimaria + semanaderetraso );
+
+  document.write ("el acreedor debe" + deudatotal);  }
+
+
+
+else if (semanaderetraso == 3 ) 
+
+ { semanaderetraso= (deudaprimaria*30)/100;
+
+  deudatotal = ( deudaprimaria + semanaderetraso );
+
+  document.write ("el acreedor debe" + deudatotal); }
+
+
+
+else if (semanaderetraso == 4 || semanaderetraso == "1 mes")
+
+{semanaderetraso= (deudaprimaria*40)/100 
+
+  deudatotal = ( deudaprimaria + semanaderetraso );
+
+  document.write ("el acreedor debe" + deudatotal); }
+
+
+
+
+else if (semanaderetraso == 5)
+
+{semanaderetraso= (deudaprimaria*50)/100 
+
+  deudatotal = ( deudaprimaria + semanaderetraso );
+
+  document.write ("el acreedor debe" + deudatotal); }
+
+
+
+else if (semanaderetraso == 6) 
+{ semanaderetraso = (deudaprimaria * 60 ) / 100
+
+  deudatotal = ( deudaprimaria + semanaderetraso );
+
+  document.write ("el acreedor debe" + deudatotal)  }
+
+
+
+ else if (semanaderetraso == 7 )
+
+  { semanaderetraso = (deudaprimaria*70)/100;
+    
+  deudatotal = ( deudaprimaria + semanaderetraso );
+  
+  document.write ("el acreedor debe" + deudatotal); }
+
+
+ 
+  
+else if (semanaderetraso == 8 || semanaderetraso =="dos meses"  )
+
+{ semanaderetraso = (deudaprimaria*80)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+
+
+ 
+
+else if (semanaderetraso == 9 )
+
+{ semanaderetraso = (deudaprimaria*90)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+
+
+ 
+
+else if (semanaderetraso == 10 )
+
+{ semanaderetraso = (deudaprimaria*100)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+
+
+ 
+
+else if (semanaderetraso == 11 )
+
+{ semanaderetraso = (deudaprimaria*110)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+
+
+ 
+
+else if (semanaderetraso == 12 || semanaderetraso == "3 meses" )
+
+{ semanaderetraso = (deudaprimaria*120)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+
+
+ 
+
+else if (semanaderetraso == 13 )
+
+{ semanaderetraso = (deudaprimaria*130)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 14 )
+
+{ semanaderetraso = (deudaprimaria*140)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 15 )
+
+{ semanaderetraso = (deudaprimaria*150)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 16 || semanaderetraso == "4 meses" )
+
+{ semanaderetraso = (deudaprimaria*160)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 17 )
+
+{ semanaderetraso = (deudaprimaria*170)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 18 )
+
+{ semanaderetraso = (deudaprimaria*180)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 19 )
+
+{ semanaderetraso = (deudaprimaria*190)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 20 || semanaderetraso == "5 meses" )
+
+{ semanaderetraso = (deudaprimaria*200)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+
+else if (semanaderetraso == 21  )
+
+{ semanaderetraso = (deudaprimaria*210)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+
+else if (semanaderetraso == 22 )
+
+{ semanaderetraso = (deudaprimaria*220)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+
+else if (semanaderetraso == 23 )
+
+{ semanaderetraso = (deudaprimaria*230)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+
+else if (semanaderetraso == 24  || semanaderetraso=="6 meses")
+ 
+{ semanaderetraso = (deudaprimaria*240)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 25 )
+
+{ semanaderetraso = (deudaprimaria*250)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 26 )
+
+{ semanaderetraso = (deudaprimaria*260)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 27 )
+
+{ semanaderetraso = (deudaprimaria*270)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 28 || semanaderetraso == "7 meses" )
+
+{ semanaderetraso = (deudaprimaria*280)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 29 )
+
+{ semanaderetraso = (deudaprimaria*290)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 30 )
+
+{ semanaderetraso = (deudaprimaria*300)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 31 )
+
+{ semanaderetraso = (deudaprimaria*310)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 32 || semanaderetraso == "8 meses" )
+
+{ semanaderetraso = (deudaprimaria*320)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 33 )
+
+{ semanaderetraso = (deudaprimaria*330)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 34 )
+
+{ semanaderetraso = (deudaprimaria*340)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 35 )
+
+{ semanaderetraso = (deudaprimaria*350)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 36 || semanaderetraso == "9 meses" )
+
+{ semanaderetraso = (deudaprimaria*360)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 37 )
+
+{ semanaderetraso = (deudaprimaria*370)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 38 )
+
+{ semanaderetraso = (deudaprimaria*380)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 39 )
+
+{ semanaderetraso = (deudaprimaria*390)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 40 || semanaderetraso == "10 meses")
+
+{ semanaderetraso = (deudaprimaria*400)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 41 )
+
+{ semanaderetraso = (deudaprimaria*410)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 42 )
+
+{ semanaderetraso = (deudaprimaria*420)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 43 )
+
+{ semanaderetraso = (deudaprimaria*430)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 44 || semanaderetraso == "11 meses")
+
+{ semanaderetraso = (deudaprimaria*440)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 45 )
+
+{ semanaderetraso = (deudaprimaria*450)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 46 )
+
+{ semanaderetraso = (deudaprimaria*460)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 47 )
+
+{ semanaderetraso = (deudaprimaria*470)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else if (semanaderetraso == 48 || semanaderetraso == "un año ")
+
+{ semanaderetraso = (deudaprimaria*480)/100;
+  
+deudatotal = ( deudaprimaria + semanaderetraso );
+
+document.write ("el acreedor debe" + deudatotal); }
+ 
+
+else{ document.write("ingrese un dato")};
+
+
+  //Fin del sistema de cálculo de 10% semanal 
+
+
+
+////////////////////////////////////
+
+
+
+
+// If ( cliente=Ezequiel ||cliente=ezequiel &&semanasderetraso = 1)
+// {deudaprimaria + Semanasderetraso }
+
+
+// else if (cliente = Ezequiel || cliente = ezequiel && semanasderetraso = 0) { document.write (“esto es lo que debe Esequiel “ + deuda)
+
+// Else {}
+
